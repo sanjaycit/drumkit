5313fc526fb10598c22aef9c7d97491c366b691f
sanjaycit <133189814+sanjaycit@users.noreply.github.com>
2023-09-29 22:38:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+var numberOfButtons=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfButtons;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",function()
+    {
+        var insideHTML=this.innerHTML;
+        makeSounds(insideHTML);
+        
+    })
+}
+document.addEventListener("keypress",function(event){
+    makeSounds(event.key);
+});
+function makeSounds(key)
+{
+    switch(key)
+        {
+            case "w":
+                var crash=new Audio('crash.mp3');
+                crash.play();
+                break;
+            case "a":
+                var kick=new Audio('kick-bass.mp3');
+                kick.play();
+                break;
+            case "s":
+                var snare=new Audio("snare.mp3");
+                snare.play();
+                break;
+            case "d":
+                var tom1 =new Audio('tom-1.mp3');
+                tom1.play();
+                break;
+            case "j":
+                var tom2=new Audio('tom-2.mp3');
+                tom2.play();
+                break;
+            case "k":
+                var tom3=new Audio('tom-3.mp3');
+                tom3.play();
+                break;
+            case "l":
+                var tom4=new Audio('tom-4.mp3');
+                tom4.play();
+                break;
+        }
+
+}
